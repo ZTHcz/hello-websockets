5ed0162aa0ccc60f5b952b24a0254230578765fa
ZTHcz <zachthart@hotmail.com>
2020-01-30 19:44:29 -0600

    failed magic sauce

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({server:server});
 wss.on('connection', function(ws,request){
     console.long('connected!');
+    ws.on('message',function(message){
+        wss.clients.forEach(function(client){
+            if(client.readyState === WebSocket.OPEN){
+                client.send(message);
+            }
+        });
+    });
 });
 
 server.listen(8080,function(){
